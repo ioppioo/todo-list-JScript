349f37194113dc74f7c0b765a7ccbc9a7e018431
ioppioo <4VgqmaQ37a9GYd4>
2022-05-08 17:54:11 +0300

    Добавлено поле ввода

--- script/script.js
+++ script/script.js
@@ -1,32 +1,71 @@
-// кнопка редактирования задачи
+// добавляет кнопку редактирования задачи и событие на кнопку с вызовом функции
 
-let tasks = document.querySelectorAll("ol > li");
+let tasks = document.querySelectorAll(".tasks__task");
 
-for (let task of tasks) {
-    task.insertAdjacentHTML('beforeend', '<button class="button button-task-redact" >✎</button>');
-        task.onclick = function () {
-        }
+function createInput(text) {
+    let input = document.createElement('input');
+    input.value = text;
+
+    return input;
 }
 
-// кнопка редактирования заголовка задачи
+function replaceTaskWithInput (task) {
+    let taskText = task.querySelector('.tasks__task-text').innerText;
+    let input = createInput(taskText);
+    task.innerHTML = '';
+    task.appendChild(input);
+}
 
-let titleTasks = document.querySelectorAll(".title-note");
+function createRedactTaskButton() {
+    let redactButton = document.createElement('button');
+    redactButton.className = 'button button-task-redact';
+    redactButton.innerText = '✎';
+    redactButton.onclick = function (event) {
+        event.stopPropagation();
+        let task = event.target.parentElement;
+        replaceTaskWithInput(task);
+    }
 
-for (let title of titleTasks) {
-    title.insertAdjacentHTML('beforeend', '<button class="button button-title-redact" >✎</button>');
-    title.onclick = function () {
+    return redactButton;
+}
+
+
+// подтверждение выполнения задачи
+for (let task of tasks) {
+    task.appendChild(createRedactTaskButton());
+    task.onclick = function () {
+        task.classList.toggle('done'); //добавляет класс
     }
 }
 
-// добавлет кнопку создания новой задачи
 
-let notes = document.querySelectorAll("ol > li:last-child");
-
-for (let newTask of notes) {
-    newTask.insertAdjacentHTML('beforeend', '<button class="button button-task-new">+</button>');
-    newTask.lastChild.onclick = function () {
-        }
-}
+// let tasks = document.querySelectorAll("ol > li");
+//
+// for (let task of tasks) {
+//     task.insertAdjacentHTML('beforeend', '<button class="button button-task-redact" >✎</button>');
+//     task.onclick = function () {
+//     }
+// }
+//
+// // кнопка редактирования заголовка задачи
+//
+// let titleTasks = document.querySelectorAll(".title-note");
+//
+// for (let title of titleTasks) {
+//     title.insertAdjacentHTML('beforeend', '<button class="button button-title-redact" >✎</button>');
+//     title.onclick = function () {
+//     }
+// }
+//
+// // добавлет кнопку создания новой задачи
+//
+// let notes = document.querySelectorAll("ol > li:last-child");
+//
+// for (let newTask of notes) {
+//     newTask.insertAdjacentHTML('beforeend', '<button class="button button-task-new">+</button>');
+//     newTask.lastChild.onclick = function () {
+//     }
+// }
 
 // кнопка удаления заметки
 
@@ -38,14 +77,3 @@ for (let taskDel of note) {
         taskDel.remove();
     }
 }
-
-// для создания новой заметки
-
-let newNote = document.querySelector('#get-note');
-    newNote.onclick = function getNote () {
-    }
-
-
-
-
-
